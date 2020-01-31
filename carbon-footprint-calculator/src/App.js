3946ecda659201e1b4d889f5ec8fdcd75350d4a1
Jennifer Stephan <jennifer.stephan@edu.escpeurope.eu>
2020-01-31 14:25:51 +0100

    react components for questions, questions count, answer options when radio buttons and quiz to display every questions ok

--- carbon-footprint-calculator/src/App.js
+++ carbon-footprint-calculator/src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import {render} from 'react-dom';
-import Question from './Question';
 import './App.css';
 
 class App extends React.Component {
+
+  constructor(props) {
+  super(props);
+
+  this.state = {
+    counter: 0,
+    questionId: 1,
+    question: '',
+    answerOptions: [],
+    answer: '',
+    answersCount: {},
+    result: ''
+  };
+}
+
   render() {
     return (
       <div className="App">
